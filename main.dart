80e3ff83b2b5451f6ff19c63378d0ed27421483a
dribbs90 <christhfc90@gmail.com>
2025-07-21 00:17:26 +0100

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,10 +1,25 @@
-import 'package:flame/flame.dart';
-import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
-import 'isometric_game.dart';
 
 void main() {
-  WidgetsFlutterBinding.ensureInitialized();
-  Flame.device.setPortrait();
-  runApp(GameWidget(game: IsometricGame()));
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Medieval Tower Defense',
+      home: Scaffold(
+        backgroundColor: Colors.brown[100],
+        body: Center(
+          child: Text(
+            'Medieval Tower Defense',
+            style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+          ),
+        ),
+      ),
+    );
+  }
 }
