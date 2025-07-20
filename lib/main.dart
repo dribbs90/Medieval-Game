b6b74621d193f8a6333a35f5fbff547352ec6d89
dribbs90 <221886692+dribbs90@users.noreply.github.com>
2025-07-21 00:48:16 +0100

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,12 @@
 
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+      body: Center(
+        child: Text('Medieval Tower Defense'),
+      ),
+    ),
+  ));
+}
